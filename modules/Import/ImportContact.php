0b7167aca9ff78e938faefdc6d575f02723a98e7
dfar2008 <dfar2008@gmail.com>
2013-08-27 16:48:43 +0800

    导入联系人有打印代码

--- modules/Import/ImportContact.php
+++ modules/Import/ImportContact.php
@@ -1,22 +1,4 @@
 <?php
-/*********************************************************************************
- * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
- * ("License"); You may not use this file except in compliance with the 
- * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
- * Software distributed under the License is distributed on an  "AS IS"  basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- * The Original Code is:  SugarCRM Open Source
- * The Initial Developer of the Original Code is SugarCRM, Inc.
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
- * All Rights Reserved.
- * Contributor(s): ______________________________________.
- ********************************************************************************/
-/*********************************************************************************
- * $Header$
- * Description:  TODO: To be written.
- ********************************************************************************/
-include_once('config.php');
 require_once('include/logging.php');
 require_once('modules/Contacts/Contacts.php');
 require_once('modules/Import/UsersLastImport.php');
@@ -269,10 +251,10 @@ class ImportContact extends Contacts {
 			  }		 
 		  }
 
-		  //$sql1 = "insert into ".$table_name." (".$column.",smcreatorid,createdtime,modifiedtime) values(".$value.",'".$current_user->id."',".$this->db->formatDate($createdtime).",".$this->db->formatDate($modifiedtime).")";
+		  $sql1 = "insert into ".$table_name." (".$column.",smcreatorid,createdtime,modifiedtime) values(".$value.",'".$current_user->id."',".$this->db->formatDate($createdtime).",".$this->db->formatDate($modifiedtime).")";
 
-          $sql1 = "insert into ".$table_name." (".$column.",createdtime,modifiedtime) values(".$value.",".$this->db->formatDate($createdtime).",".$this->db->formatDate($modifiedtime).")";
-          var_dump($sql1);
+          //$sql1 = "insert into ".$table_name." (".$column.",createdtime,modifiedtime) values(".$value.",".$this->db->formatDate($createdtime).",".$this->db->formatDate($modifiedtime).")";
+          //var_dump($sql1);
 		  $eof = $this->db->query($sql1); 
 	  }
       	
